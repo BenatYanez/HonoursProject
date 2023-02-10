353236f7eba4cf6034e18d42ddab17319eb2b80b
BenatYanez <125028091+BenatYanez@users.noreply.github.com>
2023-02-10 11:45:28 +0000

    Update hstatistics_script.R

--- scripts/hstatistics_script.R
+++ scripts/hstatistics_script.R
@@ -1,18 +1,20 @@
+#Modified from Matthew Fellbaum
+#Last modified 10/02/2023 
 ## Calculate H statistics for all rows in the hapcount file using 2 loops.
 
 # Set working directory
 #setwd("~/Documents/Uni/4th_year/Honours_Project/Coding")
 
-# Open hapcount table into R 
-hapcount_table <- read.table("haplotype_count_0.98MB.hapcount", 
+# Open hapcount table into R, put name of the file in "file name"
+hapcount_table <- read.table("file name", 
                              fill = T, skip = 1, col.names = c(1:13))
 
-# Open recombination table and add recombination column to hapcount table
+# Open recombination table and add recombination column to hapcount table.
 rec_table <- read.table("Rec_AT_098MB.dat")
 rec_vector <- rec_table[["cM.Mb"]]
 #Add rec_vector as the 14th collumn
 hapcount_table$x14 <- rec_vector
-#Remove NA data- For sample data this removes half the dta since the first half have NA recombination
+#Remove data with NA recombination
 hapcount_table <- na.omit(hapcount_table)
 
 
@@ -20,7 +22,7 @@ hapcount_table <- na.omit(hapcount_table)
 hapcount_table <- hapcount_table[hapcount_table[,4] >= 100,]
 hapcount_table <- hapcount_table[hapcount_table[,14] >= 0.5,]
 
-# Remove the centromere
+# Remove the centromere, chromosme dependant. In this case for chromosome 1
 #hapcount_table_1 <- hapcount_table[hapcount_table[,3] <= 13700000,]
 #hapcount_table_2 <- hapcount_table[hapcount_table[,2] >= 15900000,]
 #hapcount_table <- rbind(x = hapcount_table_1, y = hapcount_table_2)
@@ -52,7 +54,7 @@ for(n in loop.vector.1){
   hapfreq <- rep(NA, length(loop.vector.2))
   multiplicity <- rep(NA, length(loop.vector.2))
   
-  # Open inner loop for 1 row  
+  # Open inner loop for 1 row. Each row represents one 20kb window with recombination data  
   for(f in loop.vector.2){  #For each collumn
     
     # Extract haplotype frequency and multiplicity from hapcount table    
@@ -64,11 +66,11 @@ for(n in loop.vector.1){
     j <- ((d[2]/90)^2)*(d[1])  #d[2] is the number of individuals d[1] is the number of haplotypes with that many individuals
     h1.single.row[(f-6)] <- j #stores this calculation in vector
     
-    # Define hapfreq and multiplicity vectors for 1 row
+    # Define hapfreq and multiplicity vectors for 1 window
     hapfreq[f-6] <- d[2]
     multiplicity[f-6] <- d[1]
      
-  } #Done the calculation in every collumn for a single row
+  } #Done the calculation in every collumn for a single window
   
   # Reverse hapfreq and multiplicity vectors 
   hapfreq <- rev(hapfreq)
@@ -76,18 +78,18 @@ for(n in loop.vector.1){
   multiplicity <- rev(multiplicity)
   multiplicity <- multiplicity[!is.na(multiplicity)]
   
-  # Define 2 most common haplotypes for 1 row 
+  # Define 2 most common haplotypes for 1 window 
   hapfreq1 <- 0
   hapfreq2 <- 0
   
   hapfreq1 <- hapfreq[1] 
   if(multiplicity[1] == 1) {hapfreq2 <- hapfreq[2]} 
   #E.g if most common haplotype was 1:16 then to obtain hapfreq2 go to second most common 1:10.
-  #If most common haplotype was 2:16 then obtain hapfreq2 agin from the 2:16 since there are two haplotypes with equal number of people
+  #If most common haplotype was 2:16 then obtain hapfreq2 again from the 2:16 since there are two haplotypes with equal number of people
   else {hapfreq2 <- hapfreq[1]}
   if(is.na(hapfreq2)) {hapfreq2 <- 0}
   
-  # Calculate H1, H12, H2 and H2/H1 for all rows
+  # Calculate H1, H12, H2 and H2/H1 for all windows
   h1[n] <- sum(h1.single.row, na.rm = T)
   h12[n] <- h1[n] + 2*(hapfreq1/90)*(hapfreq2/90) 
   h2[n] <- h1[n]-((hapfreq1/90)^2)
@@ -113,7 +115,7 @@ h1_table <- h1_table[h1_table[, 15] >= h1_quantile,]
 h12_table <- cbind(hapcount_table, h12, h21)
 h12_table <- h12_table[h12_table[, 15] >= h12_quantile,]
 
-# Plotting
+# Plot H1, H12 and H2/H1 statistics across the windows with a Bonferroni significance line
 
 plot(x = midpoint, y = h1, pch = 20, xlab = "Window midpoint (Mb)", main = "H1 with Bonferroni significance line", ylab = "H1")
 abline(h = h1_quantile, lty = 3)
@@ -126,7 +128,7 @@ abline(h = h21_quantile, lty = 3)
 
 
 
-
+#Plot H1, H12 and H2/H1 against recombination rate.
 plot(x = hapcount_table$x14, y = h1, pch = 20, xlab = "Recombination rate (cM/Mb)", main = "H1 as a function of recombination rate", ylab = "H1")
 plot(x = hapcount_table$x14, y = h12, pch = 20, xlab = "Recombination rate (cM/Mb)", main = "H12 as a function of recombination rate", ylab = "H12")
 plot(x = hapcount_table$x14, y = h21, pch = 20, xlab = "Recombination rate (cM/Mb)", main = "H2/H1 as a function of recombination rate", ylab = "H2/H1")
