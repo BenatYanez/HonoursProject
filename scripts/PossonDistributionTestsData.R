990500ace987f59a32306a0f9e42b80a17de9ee9
BenatYanez <125028091+BenatYanez@users.noreply.github.com>
2023-02-10 11:34:08 +0000

    Update PossonDistributionTestsData.R

--- scripts/PossonDistributionTestsData.R
+++ scripts/PossonDistributionTestsData.R
@@ -1,25 +1,29 @@
-read.table ("1Mbfiltered.ldepth.mean", header=T)
-#Open up the Depth file and put it into a R compatible table
-depth_table <-read.table ("1Mbfiltered.ldepth.mean", header=T)
+#Modified from Matthew Fellbaum
+#Last modified 10/02/2023
+#Put the name of the file on "file name"
+read.table ("file name", header=T)
+#Open up the Depth file and put it into a R compatible table, put the name of the file on "file name"
+depth_table <-read.table ("file name", header=T)
 #Calculate mean,var and density
 depth <- density (depth_table$MEAN_DEPTH)
 mean <- mean (depth_table$MEAN_DEPTH)
 var <- var (depth_table$MEAN_DEPTH)
 qpois (c(0.0025, 0.9975),mean)
 
-#Plot the Poisson Distribution
+#Plot the mean site depth data
 plot(depth,frame =FALSE, col ="red",
-     main = "Density plot of mean site depth of 1Mb data",
+     main = "Density plot of mean site depth of the entire genome",
      xlim = c(10,80),
      xlab = "Mean site depth" )
+#Plot a line showing the Poisson curve
 lines (dpois (seq(0, max(depth_table$MEAN_DEPTH),1),mean),
        col = "green", lty =2 )
-#Changed from original code from 0.005 to 0.0025 and from 0.995 to 0.9975 
+#Add vertical lines representing the 99.5% confidence intervals and add a legend for the 3 lines
 abline (v = qpois(c(0.0025,0.9975), mean), col=c("blue", "blue"),
         lty =3:3)
 legend (50, 0.06, legend=c("Actual Data", "Poisson Curve", "qpois 99.5%"),
         col=c("red", "green", "blue"), lty=1:2, cex=0.8)
-#legend (50,0.06, legend=c("ATsweeps","qpois =0.005", "qpois=0.995"), col=c("red","green","blue"), lty=1:1, cex=0.8)
+#Create a density plot
 plot(density(dpois(depth_table$MEAN_DEPTH,
                    mean(depth_table$MEAN_DEPTH))),
      xlim=c(-0.002,0.002))
