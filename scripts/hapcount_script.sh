23f6acc195fca412a1f8f56ad28e20a075f6e6a4
BenatYanez <125028091+BenatYanez@users.noreply.github.com>
2023-02-10 14:30:03 +0000

    Update hapcount_script.sh

--- scripts/hapcount_script.sh
+++ scripts/hapcount_script.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# 3rd April 2021
-# Matthew Fellbaum
-# Modification of Gertjan Bisschop's script for running array job on server
+# Modified 10/02/2023
+# 
+# Modification of Matthew Fellbaum's script
 # Obtain number of unique haplotypes as defined by BED file
 # Need to first run 'sconda atsweeps'
 
@@ -10,14 +10,14 @@
 #$ -N vcf_AT_hap									# Name of job in 'wstat' list
 #$ -V												# Pass current environment to job
 #$ -cwd												# Run file from current working directory
-#$ -l h=c2											# Run array job on this sub-server
+#$ -l h=c1											# Run array job on this sub-server
 #$ -o /data/hartfield/atsweeps/scripts/output/		# Folder for STDOUT print
 #$ -e /data/hartfield/atsweeps/scripts/error/		# Folder for STDERR print
 
 # Obtain hapcount as defined by BED file
-vcftools --vcf /data/hartfield/atsweeps/analyses/Vcf_subset_AT.vcf --hapcount hapcount_BED_file_20kb.bed --chr 1 --min-alleles 2 --max-alleles 2 --remove-indels --max-missing 1 --min-meanDP 17 --max-meanDP 49 --out /scratch/mfellbaum/hapcount_chr1_20kb
+vcftools --vcf /data/hartfield/atsweeps/analyses/Vcf_subset_AT.vcf --hapcount hapcount_BED_file_20kb.bed --chr 1 --min-alleles 2 --max-alleles 2 --remove-indels --max-missing 1 --min-meanDP 17 --max-meanDP 49 --out /scratch/byanez/hapcount
 
 # Copy file back to head folder
-rsync -avz /scratch/mfellbaum/hapcount_chr1_20kb.hapcount /data/hartfield/atsweeps/analyses/
+rsync -avz /scratch/byanez/hapcount.hapcount /data/hartfield/atsweeps/analyses/byanez
 
 # EOF
