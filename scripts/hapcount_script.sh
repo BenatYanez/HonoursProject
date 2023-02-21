3d12b5d6278db8edd68a85112f234f25bb1386e2
BenatYanez <125028091+BenatYanez@users.noreply.github.com>
2023-02-21 12:02:40 +0000

    Update hapcount_script.sh

--- scripts/hapcount_script.sh
+++ scripts/hapcount_script.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-# Modified 20/02/2023
-# 
+# Modified 21/02/2023
+#
 # Modification of Matthew Fellbaum's script
-# Obtain number of unique haplotypes as defined by BED file
+# Obtain number of unique haplotypes as defined by BED files for each chromosome
 # Need to first run 'sconda atsweeps'
 
 # Grid Engine options (lines prefixed with #$)
-#$ -N vcf_AT_hap									# Name of job in 'wstat' list
-#$ -V												# Pass current environment to job
-#$ -cwd											# Run file from current working directory
-#$ -l h=c1									# Run array job on this sub-server
+#$ -N vcf_hap_count                                                                     # Name of job in 'wstat' list
+#$ -V                                                                                           # Pass current environment to job
+#$ -cwd                                                                                 # Run file from current working directory
+#$ -l h=c1                                                                      # Run array job on this sub-server
 #$ -t 1-5                   # Run command for each chromosome
-#$ -o /data/hartfield/atsweeps/scripts/output/		# Folder for STDOUT print
-#$ -e /data/hartfield/atsweeps/scripts/error/		# Folder for STDERR print
+#$ -o /data/hartfield/atsweeps/scripts/output/          # Folder for STDOUT print
+#$ -e /data/hartfield/atsweeps/scripts/error/           # Folder for STDERR print
 
 #Create a variable to carry out the script for every chromosome
-chrmnm =${SGE_TASK_ID}
+chrmnm=${SGE_TASK_ID}
 # Obtain hapcount as defined by BED file
-vcftools --vcf /data/hartfield/atsweeps/analyses/Vcf_subset_AT.vcf --hapcount hapcount_BED_file_chrom${chrmnm}_20kb.bed --chr ${chrmnm} --min-alleles 2 --max-alleles 2 --remove-indels --max-missing 1 --min-meanDP 17 --max-meanDP 49 
---out /scratch/byanez/hapcount_chromosome_${chrmnm}
+vcftools --vcf /data/hartfield/atsweeps/analyses/Vcf_subset_AT.vcf --hapcount /data/hartfield/atsweeps/analyses/byanez/bedfiles/hapcount_BED_file_chrom"$chrmnm"_20kb.bed --chr $chrmnm \
+--min-alleles 2 --max-alleles 2 --remove-indels --max-missing 1 --min-meanDP 17 --max-meanDP 49 --out /scratch/byanez/hapcount_chromosome_"$chrmnm"
 
 # Copy file back to head folder
-rsync -avz /scratch/byanez/hapcount_chromosome_${chrmnm}.hapcount /data/hartfield/atsweeps/analyses/byanez/hapcount
+rsync -avz /scratch/byanez/hapcount_chromosome_"$chrmnm".hapcount /data/hartfield/atsweeps/analyses/byanez/hapcount
 
 # EOF
