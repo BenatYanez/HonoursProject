4ad3d858f2f84c6d35696fe08c7b11725e437469
BenatYanez <125028091+BenatYanez@users.noreply.github.com>
2023-03-28 17:14:00 +0100

    Update haploidconversion.py

--- scripts/haploidconversion.py
+++ scripts/haploidconversion.py
@@ -1,1 +1,16 @@
+import sys
+chromosome = int(sys.argv[1])
+hap_to_dip = {'0':'0/0', '1':'1/1'}
+
+with open(f'data/hartfield/atsweeps/analysis/byanez/VCF/Vcf_Chr{chromosome}_Neutral.vcf',"r") as file, open("/scratch/byanez/Vcf_Chr5_Neutral_Diploid.vcf","w") as s_file:
+        for line in file:
+                if line[0] == '#':
+                        s_file.write(line)
+                else:
+                        rowstr = line.split('\t')[9:]
+                        for i in range(len(rowstr)):
+                                rowstr[i] = hap_to_dip[rowstr[i][0]] + rowstr[i][1:]
+                                print(rowstr)
+                        row = '\t'.join(str(line).split('\t')[:9]) + '\t' + '\t'.join(rowstr)
+                        s_file.write(row)
 
