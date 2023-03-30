e8d9d2df3f5fe6b745ad672e2e14c553b4f42879
BenatYanez <125028091+BenatYanez@users.noreply.github.com>
2023-03-30 11:50:55 +0100

    Update haploidconversion.py

--- scripts/haploidconversion.py
+++ scripts/haploidconversion.py
@@ -1,8 +1,12 @@
 import sys
 chromosome = int(sys.argv[1])
+repeat = int(sys.argv[2])
 hap_to_dip = {'0':'0/0', '1':'1/1'}
+directory= '/scratch/byanez/'
+vcf_file_name = f'Vcf_Chr{chromosome}_Neutral_Diploid_repeat{repeat}'
+complete_file = os.path.join(directory,vcf_file_name)
 
-with open(f'data/hartfield/atsweeps/analysis/byanez/VCF/Vcf_Chr{chromosome}_Neutral.vcf',"r") as file, open("/scratch/byanez/Vcf_Chr5_Neutral_Diploid.vcf","w") as s_file:
+with open(f'data/hartfield/atsweeps/analysis/byanez/VCF/Vcf_Chr{chromosome}_Neutral_repeat{repeat}.vcf',"r") as file, open(f'{complete_file}.vcf',"w") as s_file:
         for line in file:
                 if line[0] == '#':
                         s_file.write(line)
